1a22b82868f3d97baab2db1276709b70b097ea4d
ci-mustafa <DELETED-1713466147100>
2024-04-30 11:22:53 +0000

    feat: complate logic of else block inside checkUSerGuess function

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,10 @@ const ColorCodeList = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b
 // Define a variable to store the generated color
 let generatedColor;
 
+// maximum attemps if user guess not correctly
+let attempts = 7;
+
+
 // wait the DOM to finish loading before running the game
 document.addEventListener("DOMContentLoaded", function () {
     let startButton = document.getElementById("start-button");
@@ -54,7 +58,6 @@ function checkUserGuess() {
 
         // store input user in a variable
         let userGuess = getUserGuess();
-
         // validate user input
         if (userGuess.length === 0) {
             let valiText = document.getElementById("validation-text");
@@ -64,10 +67,23 @@ function checkUserGuess() {
             let valiText = document.getElementById("validation-text");
             valiText.textContent = "Ensure color code length is 7";
             valiText.style.display = "block";
-        } else if (! userGuess.startsWith("#")) {
+        } else if (!userGuess.startsWith("#")) {
             let valiText = document.getElementById("validation-text");
             valiText.textContent = "Color code must start with #";
             valiText.style.display = "block";
+        }
+
+        // check if attempts are exhausted
+        if (attempts === 1) {
+            startGame();
+            let scoreElement = document.getElementById("score");
+            let pInfo = document.getElementById("guess-step");
+            let attemptPara = document.getElementById("attempt-p");
+            scoreElement.textContent = 0;
+            pInfo.style.display = "none";
+            attemptPara.style.display = "none";
+            document.getElementById("guess-box").value = "";
+            return; // Exit the function if attempts are 0
         }
 
         // check user guess against generated color
@@ -76,7 +92,37 @@ function checkUserGuess() {
             startGame(); // generate a new color
             document.getElementById("guess-box").value = "";
         } else {
-            // alert("Oops! That's not the correct color. Try again!");
+            // attempt decrement
+            attempts --;
+
+            // array to store correct guesses
+            let correctGuesses = [];
+            for (let i = 0; i < userGuess.length; i++) {
+                if (generatedColor[i] === userGuess[i]) {
+                    correctGuesses.push([i, userGuess[i]]);
+                }
+            }
+
+            // get p and span elements by id
+            let pInfo = document.getElementById("guess-step");
+            let pIndex = document.getElementById("index");
+            let pValue = document.getElementById("value");
+            let attemptPara = document.getElementById("attempt-p");
+            let attemptValue = document.getElementById("attempt");
+
+            // Clear previous entries
+            pIndex.textContent = "";
+            pValue.textContent = "";
+
+            // assign p index and p value to correct guesses inside array
+            for (let i = 0; i < correctGuesses.length; i++) {
+                pIndex.textContent += correctGuesses[i][0] + ", ";
+                pValue.textContent += correctGuesses[i][1] + ", ";
+            }
+            attemptValue.textContent = attempts;
+            attemptPara.style.display = "block";
+            pInfo.style.display = "block";
+
 
         }
     });
