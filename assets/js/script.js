cb93b485d0f2bc975adcf56c14bf4a76ef3294de
ci-mustafa <DELETED-1713466147100>
2024-05-06 19:37:16 +0000

    mod: increase attempts value

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,7 @@ const ColorCodeList = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b
 let generatedColor;
 
 // maximum attemps if user guess not correctly
-let attempts = 7;
+let attempts = 10;
 
 
 // wait the DOM to finish loading before running the game
@@ -39,7 +39,7 @@ function startGame() {
     }
 
     // change attempts to defualt
-    attempts = 7;
+    attempts = 10;
 
     // access validation text
     let valiText = document.getElementById("validation-text");
